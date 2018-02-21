a9ecd4b3fed142163ca6e92294d4912537df0feb
Your Name <seytorin@yahoo.com>
2018-02-20 20:14:49 -0600

    Possible heroku fix

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'link',
+		photo: 'https://ixquick-proxy.com/do/spg/show_picture.pl?l=english&rais=1&oiu=http%3A%2F%2Fwww.slaphappylarry.com%2Fwp-content%2Fuploads%2F2017%2F11%2FSpongebob-and-Patrick.jpg&sp=20780fdc1ab0feb43b1936423356f92d',
+		scores: [
+			'3',
+			'1',
+			'3',
+			'3',
+			'5',
+			'5',
+			'3',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Scarface',
+		photo: 'https://ixquick-proxy.com/do/spg/show_picture.pl?l=english&rais=1&oiu=http%3A%2F%2Fwww.slaphappylarry.com%2Fwp-content%2Fuploads%2F2017%2F11%2FSpongebob-and-Patrick.jpg&sp=20780fdc1ab0feb43b1936423356f92d',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'3',
+			'3',
+			'3',
+			'3',
+			'2',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Spongebob',
+		photo: 'https://ixquick-proxy.com/do/spg/show_picture.pl?l=english&rais=1&oiu=http%3A%2F%2Fwww.slaphappylarry.com%2Fwp-content%2Fuploads%2F2017%2F11%2FSpongebob-and-Patrick.jpg&sp=20780fdc1ab0feb43b1936423356f92d',
+		scores: [
+			'3',
+			'3',
+			'5',
+			'3',
+			'3',
+			'5',
+			'5',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Patrick',
+		photo: 'https://ixquick-proxy.com/do/spg/show_picture.pl?l=english&rais=1&oiu=http%3A%2F%2Fwww.slaphappylarry.com%2Fwp-content%2Fuploads%2F2017%2F11%2FSpongebob-and-Patrick.jpg&sp=20780fdc1ab0feb43b1936423356f92d',
+		scores: [
+			'5',
+			'3',
+			'2',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'taco',
+		photo: 'https://ixquick-proxy.com/do/spg/show_picture.pl?l=english&rais=1&oiu=http%3A%2F%2Fwww.slaphappylarry.com%2Fwp-content%2Fuploads%2F2017%2F11%2FSpongebob-and-Patrick.jpg&sp=20780fdc1ab0feb43b1936423356f92d',
+		scores: [
+			'3',
+			'3',
+			'5',
+			'5',
+			'5',
+			'2',
+			'5',
+			'2',
+			'5',
+			'3'
+		]
+	},
+	
+];
+
+module.exports = friendsArray;
